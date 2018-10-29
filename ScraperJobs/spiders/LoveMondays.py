6d4330b3ec1edce543d6c60677c4c4cdce453ce9
michelsn1 <michelsn1@gmail.com>
2018-10-29 18:39:48 +0000

    First commit

---
+++ ScraperJobs/spiders/LoveMondays.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from datetime import datetime
+now = datetime.today()
+
+class LovemondaysSpider(scrapy.Spider):
+    name = 'LoveMondays'
+    allowed_domains = ['lovemondays.com.br']
+    start_urls = ['https://www.lovemondays.com.br/pesquisa/vaga/pagina/1?external_job_city_id=&external_job_city_name=&q=Desenvolvedor']
+
+    def parse(self, response):
+        print(response)
+        for job in response.xpath("/html/body/main/div/div/div/div/ul/li"):
+            yield {
+            'Vaga' : job.xpath("/a/div[2]/p[1]/text()").extract(),
+            'Empresa' : job.xpath("/a/div[2]/p[2]/text()").extract_first(),
+            'Cidade' : job.xpath("/html/body/main/div/div/div/div/ul/li[1]/a/div[2]/p[2]/text()")[1].extract(),
+            'data' : job.xpath("/a/span"),
+            'diadehoje' : now,
+            }
+        
+        url = response.url
+        print(url)
+        pagina = url.split('?').pop(0)
+        pagina = int(pagina.split('/').pop())
+        pagina = str(pagina +1)
+        next_page='https://www.lovemondays.com.br/pesquisa/vaga/pagina/'+pagina+'?external_job_city_id=&external_job_city_name=&q=Desenvolvedor'
+        
+        if next_page is not None:
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(next_page, callback=self.parse)
+            
+            
